ab7416ccbbaf65efd252fbff42e0ce662a4039dc
yurrrrri <yu_ri_son@naver.com>
2023-03-23 14:08:47 +0900

    [2-09] question service

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
+
     @GetMapping("/sbb")
     @ResponseBody
     public String index() {
